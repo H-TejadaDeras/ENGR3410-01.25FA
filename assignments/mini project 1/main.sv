b43161877c0a450e043c9b294920dca950b64d6b
Henry Neftaly Tejada Deras <htejada@olin.edu>
2025-09-18 06:50:49 -0400

    Cleaned Up Mini Project 1 Code

--- assignments/mini project 1/main.sv
+++ assignments/mini project 1/main.sv
@@ -1,7 +1,6 @@
 /*  Simple RGB LED - Mini Project 1
  *  Henry Tejada Deras - 09-12-2025
  */ 
-// `include "led_controller.sv"
 
 module top(
     input logic clk, 
@@ -13,64 +12,52 @@ module top(
     // Variable Declarations
     parameter BLINK_INTERVAL = 2000000; // CLK frequency is 12 MHz, so 2,000,000 cycles is 1/6 s
     logic [$clog2(BLINK_INTERVAL) - 1:0] count = 0;
-    logic red_led, green_led, blue_led;
+    parameter HIGH = 1'b0; // LED On (accounting for active-low setup)
+    parameter LOW = 1'b1; // LED Off (accounting for active-low setup)
 
     // Initial State Declarations
     initial begin
-        RBG_R = 1'b0;
-        RGB_G = 1'b1;
-        RGB_B = 1'b1;
+        RBG_R = HIGH;
+        RGB_G = LOW;
+        RGB_B = LOW;
     end
-
-    // assign RGB_R = ~red_led;
-    // assign RGB_G = ~green_led;
-    // assign RGB_B = ~blue_led;
-
-    // led_controller U1(
-    // .i_RGB_R(~RGB_R), 
-    // .i_RGB_G(~RGB_G), 
-    // .i_RGB_B(~RGB_B),
-    // .o_RBG_R(red_led), 
-    // .o_RBG_G(green_led),
-    // .o_RBG_B(blue_led)
-    // );
 
     // Counter Logic
     always_ff @(posedge clk) begin
         if (count == BLINK_INTERVAL - 1) begin
             count <= 0;
 
-            if (RGB_R == 1'b0 && RGB_G == 1'b1 && RGB_B == 1'b1) begin // Red -> Yellow
-            RGB_R <= 1'b0;
-            RGB_G <= 1'b0;
-            RGB_B <= 1'b1;
+            // LED Control Logic
+            if (RGB_R == HIGH && RGB_G == LOW && RGB_B == LOW) begin // Red -> Yellow
+                RGB_R <= HIGH;
+                RGB_G <= HIGH;
+                RGB_B <= LOW;
             end
-            else if (RGB_R == 1'b0 && RGB_G == 1'b0 && RGB_B == 1'b1) begin // Yellow -> Green
-                RGB_R <= 1'b1;
-                RGB_G <= 1'b0;
-                RGB_B <= 1'b1;
+            else if (RGB_R == HIGH && RGB_G == HIGH && RGB_B == LOW) begin // Yellow -> Green
+                RGB_R <= LOW;
+                RGB_G <= HIGH;
+                RGB_B <= LOW;
             end
-            else if (RGB_R == 1'b1 && RGB_G == 1'b0 && RGB_B == 1'b1) begin // Green -> Cyan
-                RGB_R <= 1'b1;
-                RGB_G <= 1'b0;
-                RGB_B <= 1'b0;
+            else if (RGB_R == LOW && RGB_G == HIGH && RGB_B == LOW) begin // Green -> Cyan
+                RGB_R <= LOW;
+                RGB_G <= HIGH;
+                RGB_B <= HIGH;
             end
-            else if (RGB_R == 1'b1 && RGB_G == 1'b0 && RGB_B == 1'b0) begin // Cyan -> Blue
-                RGB_R <= 1'b1;
-                RGB_G <= 1'b1;
-                RGB_B <= 1'b0;
+            else if (RGB_R == LOW && RGB_G == HIGH && RGB_B == HIGH) begin // Cyan -> Blue
+                RGB_R <= LOW;
+                RGB_G <= LOW;
+                RGB_B <= HIGH;
             end
-            else if (RGB_R == 1'b1 && RGB_G == 1'b1 && RGB_B == 1'b0) begin // Blue -> Magenta
-                RGB_R <= 1'b0;
-                RGB_G <= 1'b1;
-                RGB_B <= 1'b0;
+            else if (RGB_R == LOW && RGB_G == LOW && RGB_B == HIGH) begin // Blue -> Magenta
+                RGB_R <= HIGH;
+                RGB_G <= LOW;
+                RGB_B <= HIGH;
             end
-            else if (RGB_R == 1'b0 && RGB_G == 1'b1 && RGB_B == 1'b0) begin // Magenta -> Red
-                RGB_R <= 1'b0;
-                RGB_G <= 1'b1;
-                RGB_B <= 1'b1;
+            else if (RGB_R == HIGH && RGB_G == LOW && RGB_B == HIGH) begin // Magenta -> Red
+                RGB_R <= HIGH;
+                RGB_G <= LOW;
+                RGB_B <= LOW;
             end
-
         end
         else begin
             count <= count + 1;
