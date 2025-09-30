5d5f9bd40554c1909a66a54128ea64ad69e7fc36
Henry Neftaly Tejada Deras <htejada@olin.edu>
2025-09-30 09:43:21 -0400

    Initial Mini Project 2 Files

---
+++ assignments/mini project 2/top.sv
@@ -0,0 +1,20 @@
+/*
+ * top comment
+ */
+`begin_keywords "1800-2005" // SystemVerilog-2005
+
+module top(
+    input  logic clk,
+    output logic RGB_R,
+    output logic RGB_G,
+    output logic RGB_B
+    );
+    
+    // Net Declarations
+    logic 
+
+    initial begin
+        pass
+    end
+endmodule
+`end_keywords "1800-2005" // SystemVerilog-2005
